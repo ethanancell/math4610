7b48e2ee26d612c47e9aa0dc53b405c629955833
ethanancell <ethanancell@gmail.com>
2019-09-04 11:36:17 -0600

    moved around files

--- tasks_source/single_machine_precision.c
+++ tasks_source/single_machine_precision.c
@@ -21,25 +21,6 @@ int main()
 		eps = 0.5 * eps;
 	}
 	printf("Size of float is %d\n", sizeof(float));
-
-	// Double
-	double one_d = 1.0;
-	double eps_d = 1.0;
-
-	double diff_d;
-
-	for(i=0; i<1001; i++)
-	{
-		diff_d = one_d - (one_d + eps_d);
-		printf("diff_d: %f, eps_d: %f\n", diff_d, eps_d);
-		if(diff_d == 0.0)
-		{
-			printf("Iterations: %d\n", i);
-			break;
-		}
-		eps_d = 0.5 * eps_d;
-	}
-	printf("Size of double is %d\n", sizeof(double));
-
+	
 	return 0;
 }
